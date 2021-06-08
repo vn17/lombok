017b5855aff4d003c241070b18112446cc0ea729
Vyshakh Nambiar <nambiv@amazon.com>
2021-06-08 14:13:09 +0530

    Test

--- test/ecj/SimpleTest.java
+++ test/ecj/SimpleTest.java
@@ -2,10 +2,9 @@
 public class SimpleTest {
 	private final String test;
 	private final int foo;
-	private final boolean baz;
 
 	public String bar() {
 		int val = getFoo() + 5;
-		return new SimpleTest("", 0, false).toString() + val;
+		return new SimpleTest("", 0).toString() + val;
 	}
 }
